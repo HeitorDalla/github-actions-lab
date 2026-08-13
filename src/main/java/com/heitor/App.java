cf5eee6716cded4b3967ccc2bdbefdb2c6f36d79
Heitor <heitorvillavilla@gmail.com>
2026-08-13 11:00:26 -0300

    chore: inicializacao do projeto em java

---
+++ src/main/java/com/heitor/App.java
@@ -0,0 +1,13 @@
+package com.heitor;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
